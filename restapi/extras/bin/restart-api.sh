2677da88a914e13091033826f8afe98b11c60d3a
nayananga@acerubuntu18.04 <nayanangamuhandiram@gmail.com>
2019-07-30 22:03:20 +0530

    fix: path to .env file

--- restapi/extras/bin/restart-api.sh
+++ restapi/extras/bin/restart-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "# Read environment variables."
-source ../../.env
+source .env
 
 echo -e "# Restart demo database."
 mysql -u$DB_USERNAME -p$DB_PASSWORD -h$DB_HOSTNAME -e "DROP DATABASE IF EXISTS $DB_DATABASE ; CREATE DATABASE $DB_DATABASE" 2> /dev/null
