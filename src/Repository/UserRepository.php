c9cce79a36498a8dedf9a7988cb0fe439e51596c
nayananga@ubuntu <nayanangamuhandiram@gmail.com>
2019-10-12 01:21:59 +0530

    update: code reformatted

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -24,13 +24,13 @@ class UserRepository extends BaseRepository
     {
         $query = 'INSERT INTO `user` (`google_id`, `email`, `nickname`, `image`) VALUES (:google_id, :email, :nickname, :image)';
         $statement = $this->database->prepare($query);
-        $statement->bindParam('google_id', $user["sub"]);
-        $statement->bindParam('email', $user["email"]);
-        $statement->bindParam('nickname', $user["name"]);
-        $statement->bindParam('image', $user["picture"]);
+        $statement->bindParam('google_id', $user->google_id);
+        $statement->bindParam('email', $user->email);
+        $statement->bindParam('nickname', $user->nickname);
+        $statement->bindParam('image', $user->image);
         $statement->execute();
 
-        return $this->checkUserByGoogleId($user["sub"]);
+        return $this->checkUserByGoogleId($user->google_id);
     }
 
     public function checkUserByGoogleId(string $google_id)
