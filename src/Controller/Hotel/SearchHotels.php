b77fb56cb99faa4ed6843d9567c102a7d0b7b211
nayananga@ubuntu <nayanangamuhandiram@gmail.com>
2019-11-04 20:52:00 +0530

    new: caching hotels ok

--- src/Controller/Hotel/SearchHotels.php
+++ src/Controller/Hotel/SearchHotels.php
@@ -10,7 +10,27 @@ class SearchHotels extends BaseHotel
     public function __invoke(Request $request, Response $response, array $args): Response
     {
         $this->setParams($request, $response, $args);
-        $hotels = $this->getHotelService()->searchHotels($this->args['query']);
+        if ($this->useRedis() === true) {
+            $hotels = $this->getHotelFromCache($this->args['query']);
+        } else {
+            $hotels = $this->getHotelService()->searchHotels($this->args['query']);
+        }
+        
         return $this->jsonResponse('success', $hotels, 200);
+    }
+
+    /**
+     * @param String $hotel_query
+     * @return mixed
+     */
+    private function getComplaintFromCache($hotel_query)
+    {
+        $hotels = $this->getFromCache($hotel_query);
+        if ($hotels === null) {
+            $hotels = $this->getHotelService()->searchHotels($hotel_query);
+            $this->saveInCache($hotel_query, $hotels);
+        }
+
+        return $hotels;
     }
 }
